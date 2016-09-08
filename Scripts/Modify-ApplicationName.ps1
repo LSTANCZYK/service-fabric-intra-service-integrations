bb9790a55449f5be2ddd7539f89b2314b4eb1a2a
Senthuran Sivananthan [MSFT] <Senthuran.Sivananthan@microsoft.com>
2016-09-07 20:11:21 -0400

    Moved utility script to common scripts folder

---
+++ Scripts/Modify-ApplicationName.ps1
@@ -0,0 +1,13 @@
+﻿#
+# Modifies the Application Name in ApplicationParameters/Cloud.xml to include the version number found in the ApplicationManifest.xml
+# Run this script from the root of the project
+
+$manifestFileName = "$pwd\ApplicationPackageRoot\ApplicationManifest.xml"
+$cloudFileName = "$pwd\ApplicationParameters\Cloud.xml"
+
+[xml]$manifest = get-content $manifestFileName
+[xml]$cloudConfig = get-content $cloudFileName
+
+$cloudConfig.Application.Name = $cloudConfig.Application.Name + '-' + $manifest.ApplicationManifest.ApplicationTypeVersion
+$cloudConfig.Save($cloudFileName)
+
